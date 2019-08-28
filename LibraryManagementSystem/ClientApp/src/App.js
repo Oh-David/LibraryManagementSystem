447b609d26ca6a5122b1c63056b667c6c573802a
Oh-David <DavidOh@protonmail.com>
2019-08-28 13:29:12 -0700

    Add route test.js for test

--- LibraryManagementSystem/ClientApp/src/App.js
+++ LibraryManagementSystem/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import { Test } from './components/Test';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
@@ -12,6 +13,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
+        <Route exact path='/test' component={Test} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetchdata' component={FetchData} />
       </Layout>
---
+++ LibraryManagementSystem/ClientApp/src/components/Test.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Test extends Component {
+    displayName = Test.name
+
+    render() {
+        return (
+            <div>
+                <h1>Hello world from test page!</h1>
+            </div>
+        );
+    }
+}
